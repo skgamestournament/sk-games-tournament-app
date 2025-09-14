93f936ee5b14baab5d23f1ca4d8746c9daf45c61
skgamestournament <skgamestournament@gmail.com>
2025-09-14 18:09:54 +0530

    Create main_layout.dart

---
+++ lib/main_layout.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'my_tournaments_page.dart';
+import 'profile_page.dart';
+import 'wallet_page.dart';
+import 'main.dart';
+import 'login_page.dart';
+
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int _selectedIndex = 0;
+  
+  // List of all the pages that the bottom nav bar will control
+  static const List<Widget> _pages = <Widget>[
+    HomePage(),
+    MyTournamentsPage(),
+    WalletPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SK Games Tournament'),
+        automaticallyImplyLeading: false, // Removes the back button
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await supabase.auth.signOut();
+              if (context.mounted) {
+                // Navigate to LoginPage and remove all previous routes
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                  (route) => false,
+                );
+              }
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // To show all labels
+        backgroundColor: Colors.grey[900],
+        selectedItemColor: Colors.deepPurpleAccent,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gamepad),
+            label: 'My Matches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Wallet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
